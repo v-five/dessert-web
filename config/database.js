7234e14c28de93cbb55da5b65dd6e4e55d3572e2
v-five <vityka.vv@gmail.com>
2014-05-12 18:33:25 +0200

    added the correct link to mongolab

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
 
 	url : {
 
-		mongolab: "mongodb://dessert:pVj7tZnB@ds039467.mongolab.com:39467/dessert-web",
+		mongolab: "mongodb://dessert:pVj7tZnB@dds049568.mongolab.com:49568/dessert-web",
 
 		localhost: "mongodb://localhost:27017/dessert-web"
 
